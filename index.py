b4958146124ae5c85cf165a28d61d4e1a77ed302
Larry-Campos03 <usuario@hotmail.com>
2021-11-27 18:15:43 -0500

    Obteniendo la fecha

--- index.py
+++ index.py
@@ -3,15 +3,13 @@ import json
 
 def main():
     try:    
-        #url = 'https://api.covidtracking.com/v1/us/daily.json'
         url = 'https://api.covidtracking.com/v1/us/20200501.json'
         contenido = requests.get(url)
         
         if contenido.status_code == 200:
-            #convertir = contenido.json() # la variable converir se convierte en un diccionario con la funion .json
-            convertir = contenido.text
-            print(convertir)
-            #print(convertir[0].get('date', 'No se encontro ese dato'))
+            convertir = contenido.json() # la variable converir se convierte en un diccionario con la funion .json
+            print(convertir.get("date", "No encontro nada"))
+       
             
     except Exception as e:
         print(f"Este es el error encontrado: {e}")

7dd3646dacfbc316f42f23a35c0cfaa4789cf051
Larry-Campos03 <usuario@hotmail.com>
2021-11-27 01:51:08 -0500

    Agregango bloque try

--- index.py
+++ index.py
@@ -2,16 +2,19 @@ import requests
 import json
 
 def main():
-    #url = 'https://api.covidtracking.com/v1/us/daily.json'
-    url = 'https://api.covidtracking.com/v1/us/20200501.json'
-    contenido = requests.get(url)
-    
-    if contenido.status_code == 200:
-        #convertir = contenido.json() # la variable converir se convierte en un diccionario con la funion .json
-        convertir = contenido.text
-        print(convertir)
-        #print(convertir[0].get('date', 'No se encontro ese dato'))
+    try:    
+        #url = 'https://api.covidtracking.com/v1/us/daily.json'
+        url = 'https://api.covidtracking.com/v1/us/20200501.json'
+        contenido = requests.get(url)
         
+        if contenido.status_code == 200:
+            #convertir = contenido.json() # la variable converir se convierte en un diccionario con la funion .json
+            convertir = contenido.text
+            print(convertir)
+            #print(convertir[0].get('date', 'No se encontro ese dato'))
+            
+    except Exception as e:
+        print(f"Este es el error encontrado: {e}")
         
 
 if __name__ == '__main__':

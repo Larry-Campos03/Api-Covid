356215e20736311184f46de3dc876cb519bbce49
Larry-Campos03 <usuario@hotmail.com>
2021-11-27 19:27:27 -0500

    Creacion de la funcion y obteniendo las claves de la API

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-def main():
+
+def obtenerDatos():
     try:    
         url = 'https://api.covidtracking.com/v1/us/20200501.json'
         contenido = requests.get(url)
@@ -11,7 +12,26 @@ def main():
                  
     except Exception as e:
         print(f"Este es el error encontrado: {e}")
-        
+    
+    finally:
+        return convertir
+
+def imprimirDatos():
+    pass
+
+
+def main():
+    
+    menu = """
+        Bienvenido a la API coronavirus, porfavor elija la fecha para obtener los resultados de dicha fecha.
+        ingrese la fecha en el formato '20210411' año, mes y dia: """
+    fecha = input(menu)
+    datos = obtenerDatos()
+    datoFecha = datos.get('date')
+    print(datoFecha) 
+    for datoFecha in datos:
+        print(datoFecha)
+
 
 if __name__ == '__main__':
     main()
